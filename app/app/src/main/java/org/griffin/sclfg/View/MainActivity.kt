7d2997267bb82abd5071e816d9d90fa3015ae0a5
Griffin Johnson <cougargriff@gmail.com>
2020-03-25 16:36:40 -0700

    firestore works
    only on actual device

--- app/app/src/main/java/org/griffin/sclfg/View/MainActivity.kt
+++ app/app/src/main/java/org/griffin/sclfg/View/MainActivity.kt
@@ -3,11 +3,17 @@ package org.griffin.sclfg.View
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
 import org.griffin.sclfg.R
 import androidx.viewpager.widget.ViewPager
+import com.google.firebase.firestore.SetOptions
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.model.Document
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activty_main.*
 import org.griffin.sclfg.View.Tabs.ListFragment
 import org.griffin.sclfg.View.Tabs.ProfileFragment
@@ -16,16 +22,16 @@ import org.griffin.sclfg.View.Tabs.SearchFragment
 class MainActivity : AppCompatActivity()
 {
     /*
-        Firebase/Firestore Setup
+        Firebase / FireStore Setup
      */
     private lateinit var userRef : DocumentReference
+    private var auth = FirebaseAuth.getInstance()
+    private var db = Firebase.firestore
+    private val FTAG = "FIRESTORE -> "
 
-
+    /* Fragment Frameworks */
     private lateinit var pa : PageAdapter
     // private lateinit var vm : ViewModel
-
-    private var auth = FirebaseAuth.getInstance()
-    private var db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
@@ -37,13 +43,44 @@ class MainActivity : AppCompatActivity()
 
     private fun lateSetup()
     {
+        firestoreSetup()
+        paSetup()
+        vpSetup()
+    }
+
+    private fun firestoreSetup()
+    {
+        userRef = db.collection("users")
+            .document(auth.uid.toString())
+
+        /*
+
+        val user = hashMapOf(
+            "screenName" to "cougargriff",
+            "created" to "NOW"
+        )
+
+        userRef.set(user, SetOptions.merge())
+            .addOnSuccessListener { Toast.makeText(this,
+                "Success!", Toast.LENGTH_SHORT) }
+            .addOnFailureListener { Toast.makeText(this,
+                "FAILURE !!", Toast.LENGTH_LONG) }
+
+         */
+    }
+
+    private fun paSetup()
+    {
         pa = PageAdapter(supportFragmentManager)
 
         /* Create and add fragments to page adapter */
         pa.addFragments(SearchFragment(), "Search")
         pa.addFragments(ListFragment(), "List")
         pa.addFragments(ProfileFragment(), "Profile")
+    }
 
+    private fun vpSetup()
+    {
         /* ViewPager Setup */
         viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener
         {
@@ -79,5 +116,4 @@ class MainActivity : AppCompatActivity()
         viewPager.adapter = pa
         tabLayout.setupWithViewPager(viewPager)
     }
-
 }
